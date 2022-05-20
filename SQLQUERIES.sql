a7c9a16635d8687f7d67663d0191e9eb3f3e056d
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-05-20 20:25:36 +0530

    Update SQLQUERIES.sql

--- SQLQUERIES.sql
+++ SQLQUERIES.sql
@@ -230,3 +230,44 @@ select distinct(p1.email) as Email from person p1,person p2 where p1.email=p2.em
 or
 select email from person  group by email having count(email)>1;
 
+
+
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+
+# Write your MySQL query statement below
+#select c.name Customers  from customers c where c.id not in(select customerId from orders o);
+select name as Customers from customers c left join orders o on c.id=o.customerId where o.customerId is null;
