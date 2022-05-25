4c671cf2dbaf2a535e632880cfb1d88d7ed167a5
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-05-25 20:35:13 +0530

    Update SQLQUERIES.sql

--- SQLQUERIES.sql
+++ SQLQUERIES.sql
@@ -311,3 +311,41 @@ So the result is customer_number 3.
 select  customer_number from orders group by customer_number  order by count(*) DESC limit 1;
 
 
+
+
+Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+
+
+
+# Write your MySQL query statement below
+select name from customer  where referee_id<> 2 or referee_id is null;
